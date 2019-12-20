9dba63c87fe48c66b892da52878a3f98282e9992
tinachen <https://gitlab.silkrode.com.tw/silkrode/atw_project_web.git>
2019-12-20 13:22:15 +0800

    環境處理

---
+++ todolist/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+
+    <todolist msg="Todolist" />
+  </div>
+</template>
+
+<script>
+import todolist from "./components/todolist.vue";
+
+export default {
+  name: "app",
+  components: {
+    todolist
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
